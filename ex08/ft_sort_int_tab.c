c58b068ae35c741ca24c8ea9d6f0a4537d1e711b
Yasemin Beyza Ciftci <yaciftci@k1m24s01.42istanbul.com.tr>
2024-10-29 16:47:44 +0300

    beyza

---
+++ ex08/ft_sort_int_tab.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaciftci <yaciftci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/07 12:28:09 by yaciftci          #+#    #+#             */
+/*   Updated: 2024/07/07 17:07:37 by yaciftci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+
+	while (size >= 0)
+	{
+		i = 0;
+		while (i < size - 1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				ft_swap(&tab[i], &tab[i + 1]);
+			}
+			i++;
+		}
+		size--;
+	}
+}
